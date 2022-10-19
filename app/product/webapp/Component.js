af31c0b82c75e7911e2bf96e8e56561313ac35af
Jhodel Cailan <jhodel.cailan@gmail.com>
2022-10-19 10:17:10 +0800

    Start from scratch

--- app/product/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
-    return AppComponent.extend('com.demo.product.Component', {
-        metadata: {
-            manifest: 'json'
-        }
-    });
-});
