81b67c90912ab8ab733571d77f5a359fc94a437a
Jhodel Cailan <jhodel.cailan@gmail.com>
2022-10-23 22:55:40 +0800

    Served assets from CAP backend

--- srv/server.js
+++ srv/server.js
@@ -1,4 +1,10 @@
 const cds = require("@sap/cds");
 const cov2ap = require("@sap/cds-odata-v2-adapter-proxy");
-cds.on("bootstrap", (app) => app.use(cov2ap()));
+const express = require("express");
+
+cds.on("bootstrap", (app) => {
+	app.use(cov2ap());
+	app.use("/assets", express.static("assets"));
+});
+
 module.exports = cds.server;
