c6381a1607eadc1ab6ea61d8d8be684572380b53
Jhodel Cailan <jhodel.cailan@gmail.com>
2023-04-29 14:36:15 +0800

    Added Manage Products (External Service) to launchpad

--- app/products-remote/webapp/test/integration/opaTests.qunit.js
+++ app/products-remote/webapp/test/integration/opaTests.qunit.js
@@ -9,7 +9,7 @@ sap.ui.require(
 		'use strict';
 		var JourneyRunner = new JourneyRunner({
 			// start index.html in web folder
-			launchUrl: sap.ui.require.toUrl('com/jcailan/remote/products') + '/index.html'
+			launchUrl: sap.ui.require.toUrl('com/jcailan/remote/products') + '/test/index.html'
 		});
 
 
